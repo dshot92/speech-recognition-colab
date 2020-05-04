86f58890f794b6a0d4ea5c4dcd4824fc83751f65
dshot92 <dshot92@gmail.com>
2020-05-04 14:27:24 +0200

    Increased automation, but still getting Unknown error after 3-4 5 minutes files

--- conversion_split.py
+++ conversion_split.py
@@ -1,15 +1,50 @@
-from os import path
+import os.path
+import sys
+import speech_recognition as sr
 from pydub import AudioSegment
+import time # for delay
 
-# Cutting audio into 5 minutes slices
+# Transcribe from google APIs
 
-offset = 300000 # 300000 == 5 minutes
-t1 = 0 
-t2 = offset
-for i in range(6):
-    t1 = i * offset #Works in milliseconds
-    t2 = t1 + offset
-    #print(t1,t2)
-    newAudio = AudioSegment.from_wav("ISW_a_complete.wav")
-    newAudio = newAudio[t1:t2]
-    newAudio.export('test/ISW_a_'+ str(i) + '.wav', format="wav") #Exports to a wav file in the current path.
+dir = "../20_minutes_slice/test"
+onlyfiles = next(os.walk(dir))[2] #dir is your directory path as string
+print ('Found ' + str(len([name for name in os.listdir(dir)])) + ' files to transcribe' )
+
+r = sr.Recognizer()
+
+result = [] # Output visualization
+
+for i in range(len(onlyfiles)):
+    
+    time.sleep(10) # seconds
+    
+    print('Processing File: ' + str(i))
+    file_wav = dir + '/ISW_a_'+ str(i) + '.wav'
+    
+    # use the audio file as the audio source
+    with sr.AudioFile(file_wav) as source:
+        audio = r.record(source)  # read the entire audio file
+
+    # recognize speech using Google Speech Recognition
+    try:
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # instead of `r.recognize_google(audio)`
+        
+        file = open(dir + '/Text/ISW_a_'+ str(i) + '.txt',"w+")
+        file.write(str(r.recognize_google(audio, language="it-IT") )) #ITALIAN
+        file.close()
+        
+        result.append([i, True])
+
+        #print( r.recognize_google(audio, show_all=True) ) #ENGLISH-US 
+
+    except sr.UnknownValueError:
+        result.append([i, False])
+        print('File_'+ str(i) + ' - Google Speech Recognition could not understand audio')
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+        
+for res in result:
+    print(res)
